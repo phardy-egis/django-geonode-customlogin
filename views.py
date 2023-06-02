186a57a105fc11476e83254c79a7e877a2f9f960
p.hardy <pierre.hardy@egis-group.com>
2023-06-02 11:00:49 +0200

    feat: added logo in view

--- views.py
+++ views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import login, authenticate  # add to imports
 from django.shortcuts import render, redirect
 from .forms import LoginForm
+from urllib.parse import unquote
+from geonode.themes.context_processors import custom_theme
 
 def login_page(request):
     """
     This view takes GET parameter named "next" as input and redirect user to the request page if login form is submitted sucessfully
     """
     form = LoginForm()
-    next_page = request.GET.get('next', '/')
+    next_page = unquote(request.GET.get('next', '/'))
     message = ''
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -25,4 +27,13 @@ def login_page(request):
         if request.user.is_authenticated:
             return redirect(next_page)
 
-    return render(request, 'customlogin/login.html', context={'form': form, 'message': message})
+    theme = custom_theme(request)
+
+    try:
+        logo_url = theme['custom_theme'].logo.url
+        if not logo_url:
+            logo_url = '/static/mapstore/img/geonode-logo.svg'
+    except :
+        logo_url = '/static/mapstore/img/geonode-logo.svg'
+
+    return render(request, 'customlogin/login.html', context={'form': form, 'message': message, 'logo': logo_url})
